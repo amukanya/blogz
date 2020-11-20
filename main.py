0bcb693beadca672893023faee5bb66550431785
bajuba <jmunger@lakelandcollege.edu>
2020-11-20 14:16:15 -0600

    query parameter thing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True   
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://buildablog:buildablog@localhost:3306/buildablog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/buildablog'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
@@ -19,39 +19,46 @@ class Blog(db.Model):
         self.body = body
 
 
-@app.route("/add", methods=['POST'])
+@app.route("/add", methods=['POST','GET'])
 def add_movie():
-    # if request.method == 'POST':
-    title_name = request.form['title']
-    body_name = request.form['body']
+    title_name = ''
+    body_name=''
+    if request.method == 'POST':
+        title_name = request.form['title']
+        body_name = request.form['body']
         
 
     
-    #initializing all the error statements
-    title_error =''
-    body_error =''
-    # checking for errors
-    if (title_name == '') and (not body_name == ''):
-        title_error = 'You did not enter the title'
-        title_name = ''
-    elif (body_name == '') and (not title_name == ''):
-        body_error = 'You did not write a blog'
-        body_name = ''
-    elif (title_name == '') and (body_name == ''):
-        title_error = 'You did not enter the title'
-        body_error = 'You did not write a blog'
-        title_name = ''
-        body_name = ''
+        #initializing all the error statements
+        title_error =''
+        body_error =''
+        # checking for errors
+        if (title_name == '') and (not body_name == ''):
+            title_error = 'You did not enter the title'
+            title_name = ''
+        elif (body_name == '') and (not title_name == ''):
+            body_error = 'You did not write a blog'
+            body_name = ''
+        elif (title_name == '') and (body_name == ''):
+            title_error = 'You did not enter the title'
+            body_error = 'You did not write a blog'
+            title_name = ''
+            body_name = ''
 
-    if (not title_error and not body_error):
-        new_blog = Blog(title_name, body_name)
-        db.session.add(new_blog)
-        db.session.commit()
-        return render_template('add-confirmation.html',new_blog=new_blog)
+        if (not title_error and not body_error):
+            new_blog = Blog(title_name, body_name)
+            db.session.add(new_blog)
+            db.session.commit()
+            return render_template('add-confirmation.html',new_blog=new_blog)
 
-    else:
-        return render_template('add.html',title_error=title_error,body_error=body_error)
+        #if we have the id query parameter
         
+        #use the id to query the database
+        return render_template('add-confirmation.html', post=post)
+
+        else:
+            return render_template('add.html',title_error=title_error,body_error=body_error)
+    
 
     
 
@@ -61,10 +68,15 @@ def add_movie():
 @app.route('/')
 def index():
 
-    
-
     blogs = Blog.query.all()
     return render_template('edit.html',title ="Build A Blog" ,blogs=blogs)
+
+# @app.route('/blog', methods=['GET','POST'])
+# def blog():
+
+#     blogs = Blog.query.all()
+#     return render_template('edit.html',title ="Build A Blog" ,blogs=blogs)
+
 
 if __name__ == "__main__":
     app.run()
